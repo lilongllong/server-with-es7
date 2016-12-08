24101888e88612e0d3f85282ebb90b7f5b77bb06
lilongllong <2453085960@qq.com>
2016-12-08 11:33:59 +0800

    change port to config port.

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 // setting app and server
+import config from "config";
 import express from "express";
 
 import app from "./lib/app";
 import httpServer from "./lib/http/server";
 
 (function startServer() {
-    app.attach(httpServer);
-    app.use(express.static("./public"));
-    app.use("/api/", require("./lib/http/api").default);
-
-    const port = process.env.PORT || 8080;
-    httpServer.listen(port, () => {
-        console.log(`Server is listening at http://localhost:${port}`);
-    });
+    checkEnvironment();
+    initApp();
+    initHttpServer();
 })();
+
+function checkEnvironment()
+{
+    const env = app.get("env");
+    if (env === "production")
+    {
+        console.log("Server is running in [production] mode.");
+    }
+    else if (env === "development")
+    {
+        console.log("Server is running in [development] mode.");
+    }
+    else
+    {
+        throw new Error("Server can be started in other env mode except development and production.")
+    }
+}
+
+function initApp()
+{
+    app.attach(httpServer);
+    if (process.env.NODE_ENV === "development")
+    {
+        app.use(express.static("./public"));
+    }
+    app.use("/api/", require("./lib.http/api").default);
+}
+
+function initHttpServer() {
+    const port = config.get("http.port");
+    console.log(port, " port");
+    httpServer.listen(port, () => {
+        console.log(`Server is listening at ${port}...`);
+    });
+}
